48e64fc40ebc4cf50de29a9165654a8caadeb48a
Rafaaaa-zzz <168199517+Rafaaaa-zzz@users.noreply.github.com>
2026-01-28 15:21:31 -0500

    fix root route remove frontend error

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+  res.send('ICEMIN API ACTIVA 🚀');
 });
 
 const sesiones = {};
